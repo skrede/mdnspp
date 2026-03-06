49426dc19cdb652afa786e5e00aec206c4d44938
skrede <code@skrede.me>
2026-03-06 06:46:24 +0100

    Progressing on clang and macos CI build issues for serve.cpp

--- example/serve.cpp
+++ example/serve.cpp
@@ -30,10 +30,11 @@ int main()
         }
     };
 
-    std::thread shutdown([&ctx] {
+    auto shutdown_task = [&ctx] {
         std::this_thread::sleep_for(std::chrono::seconds(30));
         ctx.stop();
-    });
+    };
+    std::thread shutdown(std::move(shutdown_task));
 
     std::cout << "Serving MyApp._http._tcp.local. on port 8080 (30s then auto-stop)\n";
     srv.async_start();
